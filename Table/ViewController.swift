d81410594cd3c8b25d4f1cae21f1d9bc9145d476
Dmitriy <94037443+ostermand@users.noreply.github.com>
2022-05-01 16:22:19 +0300

    first relies

--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -17,3 +17,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //создаем экземпляр ячейки, которую будем возвращать
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "contactCellIdentifier")
+        // конфигурируем ячейку
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        
+        cell.contentConfiguration = configuration
+//        // возвращаем сконфигурированный экземпляр ячейки
+        return cell
+    }
+        
+}
+
